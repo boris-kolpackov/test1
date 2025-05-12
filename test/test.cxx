bebddaf7ec64ed664a2e848b983273b8d12f59c3
Boris Kolpackov <boris@codesynthesis.com>
2025-05-12 15:41:00 +0200

    Cause warning with just -Wall

--- test/test.cxx
+++ test/test.cxx
@@ -1,16 +1,18 @@
 #include <iostream>
 
+int f ()
+{
+}
+
 int main (int argc, char* argv[])
 {
   using namespace std;
 
-#if 0
   if (argc < 2)
   {
     cerr << "error: missing name to greet" << endl;
     return 1;
   }
-#endif
 
   cout << "Hello, " << argv[1] << '!' << endl;
 }
